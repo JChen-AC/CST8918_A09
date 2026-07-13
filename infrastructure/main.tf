53e700597fa5d5fa7bbe2322c498f83a7020dfe1
JChen-AC <chen1101@algonquinlive.com>
2026-07-13 01:45:43 -0400

    formatting script

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -3,15 +3,15 @@ terraform {
   required_version = ">= 1.1.0"
 
   required_providers {
- # Azure Resource Manager provider and version
- azurerm = {
-   source  = "hashicorp/azurerm"
-   version = "~> 3.0.2"
- }
- cloudinit = {
-   source  = "hashicorp/cloudinit"
-   version = "~> 2.3"
- }
+    # Azure Resource Manager provider and version
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = "~> 2.3"
+    }
   }
 
 }
@@ -27,30 +27,30 @@ provider "cloudinit" {
 }
 
 variable "labelPrefix" {
-  type = string
+  type    = string
   default = "jccst8918"
 }
 variable "region" {
-  type = string
+  type    = string
   default = "canadacentral"
 }
 
 resource "azurerm_resource_group" "main" {
-  name  = "${var.labelPrefix}-A06-RG"
+  name     = "${var.labelPrefix}-A06-RG"
   location = var.region
   tags = {
- Class   = "CST8918"
- Assignment = "Lab"
- Lab  = "A06"
+    Class      = "CST8918"
+    Assignment = "Lab"
+    Lab        = "A06"
   }
 }
 
 resource "azurerm_storage_account" "example" {
-  name      = "${var.labelPrefix}storage"
-  resource_group_name   = azurerm_resource_group.main.name
-  location     = var.region
-  account_tier    = "Standard"
+  name                     = "${var.labelPrefix}storage"
+  resource_group_name      = azurerm_resource_group.main.name
+  location                 = var.region
+  account_tier             = "Standard"
   account_replication_type = "LRS"
-  account_kind    = "BlobStorage"
-  access_tier     = "Cool"
+  account_kind             = "BlobStorage"
+  access_tier              = "Cool"
 }
